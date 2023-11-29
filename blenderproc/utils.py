04e881e047868ee207a4aa0a0f015f20f32469c9
Markus Ernst <markusrolandernst@t-online.de>
2023-11-29 11:36:25 +0100

    added blend file

--- blenderproc/utils.py
+++ blenderproc/utils.py
@@ -1,30 +1,36 @@
 import numpy as np
 import time
+import colorsys
+from matplotlib import colors
 
-# Converts light O,W,A-I codes into RGB colors
-# using the base colors of the illuminators
-def light_code_to_color(code):
-    if code == 'O':
-        return [0, 0, 0]
-    if code == 'W':
-        return [1, 1, 1]
-    if code == 'A':
-         return [1.0, 1.0, 0.6]
-    if code == 'B':
-         return [0.678, 0.847, 0.902]
-    if code == 'C':
-         return [1, 0, 0]
-    if code == 'D':
-         return [0, 1, 0]
-    if code == 'E':
-         return [0, 0, 1]
-    if code == 'F':
-         return [1, 1, 0]
-    if code == 'G':
-         return [1, 0, 1]
-    if code == 'H':
-         return [0, 1, 1]
 
+COLOR_DICT = {
+    'B': ([0.,0.,0.], 'black'),
+    'W': ([1.,1.,1.], 'white'),
+    'R': ([1.,0,0], 'red'),
+    'B': ([0,1.,0], 'blue'),
+    'G': ([0,0,1.], 'green'),
+    'M': ([1.,0,1.], 'magenta'),
+    'C': ([0,1.,1.], 'cyan'),
+    'Y': ([1.,1.,0], 'yellow'),
+}
+
+def light_code_to_colorrgb(code):
+    return COLOR_DICT[code][0]
+    
+def light_code_to_colorname(code):
+    return COLOR_DICT[code][1]
+
+
+def scale_lightness(colorname, scale_l, scale_s):
+    # convert rgb to hls
+    h, l, s = colorsys.rgb_to_hls(*colors.to_rgb(colorname))
+    # manipulate h, l, s values and return as rgb
+    return colorsys.hls_to_rgb(h, min(1, l * scale_l),  min(1, s * scale_s))
+
+
+
+   
 # Function that calculates the rotation euler angles of each point with respect to the point (0,0,0)
 def calculate_rotation_angles(x, y, z):
     angle_x_list = []
@@ -58,41 +64,4 @@ def set_lights(light_list, light_colors, light_powers, args):
     for frame in range(args.n_frames):
         for light_id, light in enumerate(light_list):
             light.set_energy(light_powers[frame, light_id], frame=frame)
-            light.set_color(light_code_to_color(light_colors[frame][light_id]), frame=frame)
-
-    
-
-    '''
-    seed_value = int(time.time()) + frame
-    np.random.seed(seed_value)
-
-    rand_int = np.random.randint(0, 7)
-
-    arr = np.arange(1, 9)
-    np.random.shuffle(arr)
-
-    selected_numbers = arr[:2]
-    arr = np.delete(arr, [0, 1])
-
-    rand_arr = arr[:rand_int]
-    np.random.shuffle(rand_arr)
-
-    arr_energy = np.random.uniform(args.lights_min_power, args.lights_max_power, rand_int + 2)
-
-    for frame in range(args.n_frames):
-        for i, light in enumerate(light_list):
-            if light_index in rand_arr or light_index in selected_numbers:
-                light.set_energy(arr_energy[0], frame=frame)
-                seed_value = int(time.time()) + frame * (i + 2)
-                if light_index in selected_numbers:
-                    light.set_color(color_white)
-                else:
-                    np.random.seed(seed_value)
-                    np.random.shuffle(color_list)
-
-                    color_s = np.random.randint(0, len(color_list))
-                    light.set_color(color_list[color_s])
-                arr_energy = np.delete(arr_energy, 0)
-            else:
-                light.set_energy(0, frame=frame)
-    '''
+            light.set_color(light_code_to_colorrgb(light_colors[frame][light_id]), frame=frame)
